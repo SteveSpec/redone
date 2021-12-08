f7db80ba0921e1d68c670457fe5b948a487138eb
Stephen Wambua <wambuastephen001@gmail.com>
2021-12-08 09:01:31 +0300

    Update settings.py
    Guard

--- afya/settings.py
+++ afya/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-43$7e1iunhnh($_3&5pn-s7z!*ddd03sh-7_**#0p+6=#yfol@'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
